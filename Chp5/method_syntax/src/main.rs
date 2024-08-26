5c02a80351dea7f6dc67ffe0d035393348ea3e72
D-H0f <devonoffutt@gmail.com>
2024-08-25 22:36:45 -0600

    updating progress on current chapter

--- Chp5/method_syntax/src/main.rs
+++ Chp5/method_syntax/src/main.rs
@@ -21,13 +21,26 @@ impl Rectangle {
     fn width(&self) -> bool {
         self.width > 0u32
     }
-    fn can_fit(&self, &other: Rectangle) -> bool {
-        {
-            self.width > other.width && self.length > other.length
-        }
-        || self.width > other.length && self.length > other.width
+    fn can_fit(&self, other: &Rectangle) -> bool {
+        (self.width > other.width && self.height > other.height)
+            || (self.width > other.height && self.height > other.width)
     }
-}
+    // Associated Functions:
+    /* All functions defined within an impl block are called associated functions because they're
+     * associated with the type named after the impl. We can define associated functions that don't
+     * have self as their parameter, which wouldn't be methods, because they don't need an instance
+     * of the type to work with. String::from is an example of an associated function that is not a
+     * method, in this case used to construct a String.
+     * These types of fuctions are often used as constructors for their structs.
+     * */
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+    // to call this type of function we use "::"; 'let sq = Rectangle::square(3);'
+} // each struct is allowed to have multiple impl blocks, you are not limited to just one.
 fn main() {
     let rect1 = Rectangle {
         width: 30,
@@ -42,4 +55,22 @@ fn main() {
         println!("The rectangle has a nonzero width; if is {}", rect1.width);
     } // when we use .width(), Rust knows we mean the method, and when we use .width, if knows it's
       // the field
+    let rect2 = Rectangle {
+        width: 20u32,
+        height: 30u32,
+    };
+    let rect3 = Rectangle {
+        width: 40u32,
+        height: 20u32,
+    };
+    let rect4 = Rectangle {
+        width: 50u32,
+        height: 50u32,
+    };
+    println!("Can rect2 fit in rect1? {}", &rect1.can_fit(&rect2));
+    println!("Can rect3 fit in rect1? {}", &rect1.can_fit(&rect3));
+    println!("Can rect4 fit in rect1? {}", &rect1.can_fit(&rect4));
+    let square1 = Rectangle::square(20u32);
+    println!("square1 =\n{:#?}", &square1);
+    println!("can rect1 fit in square1? {}", &square1.can_fit(&rect1));
 }
